13e5da5a99ec8a1c94165ceae3b0fdb7126fcf54
babylee2002 <babylee2002@gmail.com>
2018-01-20 17:18:14 -0500

    commented lines deleted

--- server.js
+++ server.js
@@ -1,36 +1,3 @@
-// require("dotenv").config()
-
-// const express = require('express')
-// const bodyParser = require('body-parser')
-// const mongoose = require('mongoose')
-
-// const app = express()
-
-// mongoose.connect(process.env.MONGODB_URI, {
-//     useMongoClient:true
-// })
-
-// const connection = mongoose.connection
-// connection.on('connected',()=>{
-//     console.log('Mongoose Connected Successfully')
-// })
-
-// //If the connection throws and error
-// connection.on('error',(err)=>{
-//     console.log(`Mongoose default connection error:${err}`)
-// })
-// app.use(express.static(__dirname+'/client/build'))
-// app.use(bodyParser.json())
-// app.get('/',(req,res)=>{
-//     res.sendFile(`${__dirname}/client/build/index.html`)
-// })
-
-// const PORT = process.env.PORT || 3000
-// app.listen(PORT, ()=>{
-//     console.log("Magic happening on port"+PORT)
-// })
-
-
 require("dotenv").config();
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -58,8 +25,8 @@ app.get('/', (req,res) => {
     res.sendFile(__dirname + '/client/build/index.html')
   })
 
-const usersController = require('./controllers/UsersController')
-app.use('api/users',usersController)
+const users = require('./controllers/UsersController')
+app.use('api/users',users)
 
 // const ideasController = require('./controllers/IdeasController')
 // app.use('/ideas', ideasController)
