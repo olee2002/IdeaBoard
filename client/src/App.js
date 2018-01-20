164dbd1b6a2ff9763f736cc23dd499877a7d8f3e
babylee2002 <babylee2002@gmail.com>
2018-01-20 17:40:01 -0500

    Routes Added

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,32 @@
-import React, {Component} from 'react'
-// import styled from 'styled-components'
+import React, { Component } from 'react'
+import {
+  BrowserRouter as
+    Router,
+  Route,
+  Switch,
+  Link
+} from 'react-router-dom'
+import styled from 'styled-components'
 import IdeaPage from './components/IdeaPage'
 
 
 class App extends Component {
-  render(){
+  render() {
     return (
-      <div>
-        <IdeaPage/>
+      <Router>
+        <div>
+          <div>
+            <Link to='/login'>Login</Link>
+          </div>
+          <Switch>
+            <div>
+              <Route exact path = "/" render = {HomePage}/>
+              <Route exact path = "/login" render= {LogInPage}/>
+              <Route exact path = "/user/:userId" render={IdeaPage}/>
+            </div>
+          </Switch>
         </div>
+      </Router>
     )
   }
 }
