87638552bcc842e4c4bb468881c25d0663cce4e1
babylee2002 <babylee2002@gmail.com>
2018-01-20 22:04:39 -0500

    user details added

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -12,11 +12,25 @@ router.get('/', (req, res) => {
         .catch((err) => console.log(err))
 })
 
-router.post('/',(req,res)=>{
+router.post('/', (req, res) => {
     const newUser = new User(req.body.user)
-    newUser.save().then((user)=>{
-        res.json(user)
-    }).catch((err)=>{console.log(err)})
+    newUser.save()
+        .then((user) => {
+            res.json(user)
+        })
+        .catch((err) => { console.log(err) })
 })
+
+router.get('/:id', (req, res) => {
+    User
+        .findById(req.params.id)
+        .then((user) => {
+            user.ideas = user.ideas.reverse(
+                res.json(user)
+            )
+        })
+        .catch((err) => console.log(err))
+})
+
 
 module.exports = router
